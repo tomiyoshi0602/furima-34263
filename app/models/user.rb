ecda15c5fb294abc6273bb04050b760b52e76a09
tomiyoshi0602 <ytomita0602@gmail.com>
2021-04-10 17:24:46 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  with_options presence: true do
+    validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters." }
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+    validates :last_name_reading, :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+  end
 end
