50e44d9a2a5f4fe101d4afc4376bdbdd36a4b8a0
tomiyoshi0602 <ytomita0602@gmail.com>
2021-04-25 14:36:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  with_options numericality: { other_than: 1 }
-  validates :category_id, :status_id, :shipping_charge_id, 
-            :prefecture_id, :days_to_ship_id 
+  with_options presence: true do
+    validates :image, :name 
+    validates :description length: { maximum: 1000 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :status_id, :shipping_charge_id, 
+                :prefecture_id, :days_to_ship_id 
+    end
+  end
+
 end
