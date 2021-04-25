c2a07a92d5b609e3b4a4e017ec8d350994b32e8f
tomiyoshi0602 <ytomita0602@gmail.com>
2021-04-25 14:25:57 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options numericality: { other_than: 1 }
+  validates :category_id, :status_id, :shipping_charge_id, 
+            :prefecture_id, :days_to_ship_id 
 end
