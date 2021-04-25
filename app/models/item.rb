240c96689333e007a16b2c2d2e7ec4d8f7135e9c
tomiyoshi0602 <ytomita0602@gmail.com>
2021-04-25 14:45:45 +0900

    商品出品ページを表示できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image, :name 
-    validates :description length: { maximum: 1000 }
+    validates :description, length: { maximum: 1000 }
     with_options numericality: { other_than: 1 } do
       validates :category_id, :status_id, :shipping_charge_id, 
                 :prefecture_id, :days_to_ship_id 
