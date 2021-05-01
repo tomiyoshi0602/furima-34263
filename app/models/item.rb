f28c8526ff4587bea178afe1ba6e8c3aee79bf11
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-01 11:06:24 +0900

    販売手数料・販売利益の計算結果を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image, :name 
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :description, length: { maximum: 1000 }
     with_options numericality: { other_than: 0 } do
       validates :category_id, :status_id, :shipping_charge_id, 
