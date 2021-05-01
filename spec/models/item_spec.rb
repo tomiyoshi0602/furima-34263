9cc98b90f6aad9a814fde724ca243127b6374137
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-01 14:51:08 +0900

    プルリクエストの修正１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,38 +33,78 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
       end
+      it 'カテゴリーの情報が0を選択していると保存ができないこと' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
+      end
       it '商品の状態についての情報が空だと保存ができないこと' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank", 'Status is not a number')
+      end
+      it '商品の状態についての情報が0を選択していると保存ができないこと' do
+        @item.status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 0")
       end
       it '配送料の負担についての情報が空だと保存ができないこと' do
         @item.shipping_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank", 'Shipping charge is not a number')
       end
+      it '配送料の負担についての情報が0を選択していると保存ができないこと' do
+        @item.shipping_charge_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 0")
+      end
       it '発送元の地域についての情報が空だと保存ができないこと' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank", 'Prefecture is not a number')
+      end
+      it '発送元の地域についての情報が0を選択していると保存ができないこと' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
       end
       it '発送までの日数についての情報が空だと保存ができないこと' do
         @item.days_to_ship_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank", 'Days to ship is not a number')
       end
+      it '発送までの日数についての情報が0を選択していると保存ができないこと' do
+        @item.days_to_ship_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 0")
+      end
       it '販売価格が空だと保存ができないこと' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '販売価格が¥300~¥9999999の範囲以外では保存ができないこと' do
+      it '販売価格が300円未満では保存ができないこと' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
+      it '販売価格が10000000円以上では保存できないこと' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
       it '販売価格が半角数字ではければ保存ができないこと' do
-        @item.price = '１２３４'
+        @item.price = '１２３４５６'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '販売価格が半角英数混合では保存できないこと' do
+        @item.price = '123abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '販売価格が半角英語だけでは保存できないこと' do
+        @item.price = 'abcdef'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
