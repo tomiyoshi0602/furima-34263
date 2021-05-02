a750747c8413b2ba1438e2981f350cfc8bd04603
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-02 10:34:23 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order('created_at DESC')
@@ -27,7 +28,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
@@ -35,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:price, :name, :description, :image, :category_id, :status_id, :shipping_charge_id,
                                  :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
+      redirect_to action: :index
+    end
   end
 end
