1f04a36ed00d28600605c3d3bf8470072e5b58d4
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-03 11:07:52 +0900

    rubocopによる自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
-
   def index
     @items = Item.order('created_at DESC')
   end
