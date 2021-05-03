cfe41d9a5cc2e55558cba27fb6adc800b5dbba2a
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-03 11:03:27 +0900

    destroyアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
@@ -36,7 +36,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
