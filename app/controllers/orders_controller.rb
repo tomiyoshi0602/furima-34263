c64293017a440824f17a5e76741058e19c23074d
tomiyoshi0602 <ytomita0602@gmail.com>
2021-05-10 15:19:44 +0900

    before_actionにおけるアクセス制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
@@ -19,6 +21,13 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def move_to_index
+    item = Item.find(params[:item_id])
+    unless current_user.id =! item.user_id
+      redirect_to root_path
+    end
   end
 
 end
